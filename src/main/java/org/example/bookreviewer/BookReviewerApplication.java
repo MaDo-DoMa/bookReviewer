96f0b2b23a66ea822aff206f95ae8926c5769d10
Maciek Droździel <66441580+MaDo-DoMa@users.noreply.github.com>
2025-10-02 15:09:43 +0200

    add files

---
+++ src/main/java/org/example/bookreviewer/BookReviewerApplication.java
@@ -0,0 +1,13 @@
+package org.example.bookreviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookReviewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookReviewerApplication.class, args);
+	}
+
+}
